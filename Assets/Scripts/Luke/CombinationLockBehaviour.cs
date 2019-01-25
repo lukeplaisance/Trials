1531d0f105634ce1af1d45cc8fa0575f220ef880
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-01-25 16:52:37 -0600

    Added The Slot and CombinationLock scripts

---
+++ Assets/Scripts/Luke/CombinationLockBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombinationLockBehaviour : MonoBehaviour
+{
+    private List<int> answer = new List<int> { 2, 3, 1,};
+    public Slot slot1;
+    public Slot slot2;
+    public Slot slot3;
+    private bool isLocked = true;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckValues();
+	}
+
+    public void CheckValues()
+    {
+       foreach(var value in answer)
+        {
+            if (slot1.current_value == answer[0])
+                if (slot2.current_value == answer[1])
+                    if (slot3.current_value == answer[2])
+                        isLocked = false;
+        }
+    }
+
+    [ContextMenu("Rot")]
+    void Test()
+    {
+        slot1.Rotate_Slot();
+    }
+}
---
+++ Assets/Scripts/Luke/Slot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Slot : MonoBehaviour
+{
+    public int current_value = 1;
+    public Transform transform;
+
+    public void Rotate_Slot()
+    {
+        StartCoroutine("Rotate");
+
+        current_value++;
+        if(current_value > 4)
+        {
+            current_value = 1;
+        }
+    }
+
+    IEnumerator Rotate()
+    {
+        int c = 0;
+        while(c != 50)
+        {
+            transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+            c++;
+        }
+        yield return null;
+    }
+}
