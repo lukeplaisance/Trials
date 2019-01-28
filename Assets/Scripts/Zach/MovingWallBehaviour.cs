dab7e408bd9a90abb476c0efdf22019c55e6ec75
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-01-28 11:35:19 -0600

    started on moving wall behaviour

---
+++ Assets/Scripts/Zach/MovingWallBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWallBehaviour : MonoBehaviour
+{
+    public Vector3 Direction;
+    public float Speed;
+    public bool IsMoving = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(IsMoving)
+            transform.position += Direction * Speed * Time.deltaTime;
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if(hit.collider.gameObject.tag == "Terrain")
+            {
+            }
+        }
+	}
+}
