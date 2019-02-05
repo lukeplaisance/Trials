bffc9fec79f194503c2d03b82cd8a2c334f8241d
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-02-05 17:38:31 -0600

    working on player animations

---
+++ Assets/Scripts/Luke/PlayerAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+    private float v;
+    private float h;
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+	}
+
+    private void FixedUpdate()
+    {
+        animator.SetFloat("Run", v);
+    }
+}
