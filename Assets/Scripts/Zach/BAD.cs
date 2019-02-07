6d4f292ec795cee674d4d54f8b1c2b125aad5308
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-02-07 17:10:14 -0600

    last minutes change to project before final build

---
+++ Assets/Scripts/Zach/BAD.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BAD : MonoBehaviour
+{
+    public MovingWallBehaviour wall1;
+    public MovingWallBehaviour wall2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            wall1.IsMoving = true;
+            wall2.IsMoving = true;
+        }
+    }
+}
