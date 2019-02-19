16bfc498e08bfa0ea640edf56ed3a171c4b49c91
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-02-19 15:36:57 -0600

    created note scriptable object part 2

--- Assets/Scripts/Zach/NoteScriptable.cs
+++ Assets/Scripts/Zach/NoteScriptable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewNoteObject",menuName = "Zach/Note")]
+//Try try again
 public class NoteScriptable : ScriptableObject
 {
     public bool IsEnabled;
