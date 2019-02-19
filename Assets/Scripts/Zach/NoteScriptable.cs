34e64e84c8f8ddfefedb9cfcb08bf61de22cb43e
Matthew Williamson <chuwilliamson@gmail.com>
2019-02-19 16:28:17 -0600

    change the asset menu to Trials/Note

--- Assets/Scripts/Zach/NoteScriptable.cs
+++ Assets/Scripts/Zach/NoteScriptable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewNoteObject",menuName = "Zach/Note")]
+[CreateAssetMenu(menuName = "Trials/Note")]
 //Try try again
 public class NoteScriptable : ScriptableObject
 {
