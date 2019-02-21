2818fa7c55cac736522cf783487eca290e370a55
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-02-21 09:37:41 -0600

    moved scripts into "Luke" namespace

--- Assets/Scripts/Luke/PlayerAnimation.cs
+++ Assets/Scripts/Luke/PlayerAnimation.cs
@@ -3,33 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class PlayerAnimation : MonoBehaviour
+namespace Luke
 {
-    public Animator animator;
-    private float v;
-    private float h;
-	// Use this for initialization
-	void Start ()
+    public class PlayerAnimation : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        v = Input.GetAxis("Vertical");
-        h = Input.GetAxis("Horizontal");
-        var move = new Vector3(h, 0, v);
-        var speed = move.magnitude;
-        var Strafe = h;
-        animator.SetBool("IsStrafe", Strafe > 0.1 || Strafe < -0.1);
-        animator.SetBool("IsMoving", speed > 0.1 || speed < -0.1);
-        
-    }
+        public Animator animator;
+        private float v;
 
-    private void FixedUpdate()
-    {
-        animator.SetFloat("speed", v);
-        animator.SetFloat("Strafe", h);
+        private float h;
+
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
+            var move = new Vector3(h, 0, v);
+            var speed = move.magnitude;
+            var Strafe = h;
+            animator.SetBool("IsStrafe", Strafe > 0.1 || Strafe < -0.1);
+            animator.SetBool("IsMoving", speed > 0.1 || speed < -0.1);
+
+        }
+
+        private void FixedUpdate()
+        {
+            animator.SetFloat("speed", v);
+            animator.SetFloat("Strafe", h);
+        }
     }
 }
