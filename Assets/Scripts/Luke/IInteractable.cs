2a8585f349ca4daa4820b88e685c128f5d72bec0
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-02-28 14:35:27 -0600

    created interface for an interactable

---
+++ Assets/Scripts/Luke/IInteractable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Zach;
+
+namespace Luke
+{
+    public interface IInteractable
+    {
+        void Interact(Object obj);
+        IInteractor Interactor { get; set; }
+        UnityEvent Response { get; set; }
+    }
+}
