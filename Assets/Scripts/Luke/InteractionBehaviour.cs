11798ca956d86136f3c7e1bef0e9a9e6529bdb54
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-02-28 14:36:26 -0600

    implemented IInteractable interface

---
+++ Assets/Scripts/Luke/InteractionBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Zach;
+
+namespace Luke
+{
+    public class InteractionBehaviour : MonoBehaviour, IInteractable
+    {
+        public IInteractor Interactor { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public UnityEvent Response { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public void Interact(Object obj)
+        {
+            if (Interactor == null) return;
+            if (obj != Interactor) return;
+            Response.Invoke();
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Interactor = other.GetComponent<IInteractor>();
+                if (Interactor == null) return;
+
+                Interactor.SetInteraction(this);
+            }
+
+        }
+
+        public void OnTriggerExit(Collider other)
+        {
+            Interactor.ReleaseInteraction(this);
+        }
+    }
+}
