312144a85b59a66a22d36de6e41cd1d02b66fb30
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-03-12 17:22:33 -0500

    created player interact behaviour for testing interaction system

---
+++ Assets/Scripts/Zach/PlayerInteractBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Luke;
+using UnityEngine;
+
+namespace Zach
+{
+    public class PlayerInteractBehaviour : InteractorBehaviour
+    {
+        public GameEvent interactStart;
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                interactStart.Raise();
+            }
+        }
+    }
+}
