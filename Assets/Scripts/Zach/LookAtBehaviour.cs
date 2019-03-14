e2f5b2bb3709167cc0629196b01219c069eac093
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-03-14 17:59:36 -0500

    lookat
    quick look at implementation to look at a thing in update

---
+++ Assets/Scripts/Zach/LookAtBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Zach
+{
+    public class LookAtBehaviour : MonoBehaviour
+    {
+        public Transform target;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            transform.LookAt(target);
+        }
+    }
+}
