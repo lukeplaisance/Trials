629d23254fe63c99170fa2e31c8c857a3f3d091f
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-03-26 09:09:49 -0500

    started on Interaction behaviour

---
+++ Assets/Scripts/Luke/InteractableBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Luke
+{
+
+    public class InteractableBehaviour : InteractionBehaviour
+    {
+        public UnityEvent response;
+        public Animation animation;
+        public GameEvent end;
+
+        // Use this for initialization
+        void Start()
+        {
+            response = _response;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
