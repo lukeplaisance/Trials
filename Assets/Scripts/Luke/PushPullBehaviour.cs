1ae21f7eb9d87b1198363749fa00459802a9e005
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-03-28 16:51:16 -0500

    push/pull works
    When the player grabs the object, it can move the block forwards and backwards no matter which side.

--- Assets/Scripts/Luke/PushPullBehaviour.cs
+++ Assets/Scripts/Luke/PushPullBehaviour.cs
@@ -1,17 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zach;
 
 public class PushPullBehaviour : MonoBehaviour
 {
+    public float speed;
+    public NewPlayerMovementBehaviour player;
+    public GameObject front_col;
+    public GameObject back_col;
+    public GameObject left_col;
+    public GameObject right_col;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        var v = Input.GetAxis("Vertical");
+        if (player.transform.position.x == front_col.transform.position.x || 
+            player.transform.position.x == back_col.transform.position.x)
+        {
+            Move(new Vector3(0,0,v));
+        }
+
+        if (player.transform.position.x == left_col.transform.position.x ||
+            player.transform.position.x == right_col.transform.position.x)
+        {
+            Move(new Vector3(v,0,0));
+        }
+    }
+
+    public void Move(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            transform.position += direction * Time.deltaTime * speed;
+        }
+    }
 }
