822664bef864064e824938dbd71692e761fc242b
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-03-29 18:09:21 -0500

    add behaviour to raise on event on start for monobehaviour

---
+++ Assets/Scripts/Matthew/RaiseEventOnStartBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using Luke;
+using UnityEngine;
+
+public class RaiseEventOnStartBehaviour : MonoBehaviour
+{
+    public GameEvent StartEvent;
+
+    private void Start()
+    {
+        StartEvent.Raise();
+    }
+}
