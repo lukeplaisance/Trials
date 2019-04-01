b56115ff315ecfa2969d8c13b260f67f200af996
Zachary Briggs <s178020@students.aie.edu.au>
2019-04-01 15:13:39 -0500

    picking up notes after the ground working with the new refrence game object

--- Assets/Scripts/Zach/NoteBehaviour.cs
+++ Assets/Scripts/Zach/NoteBehaviour.cs
@@ -2,8 +2,29 @@
 using System.Collections.Generic;
 using Luke;
 using UnityEngine;
+using UnityEngine.UI;
+using Matthew;
 
-public class NoteBehaviour : InteractionBehaviour
+namespace Zach
 {
+    public class NoteBehaviour : MonoBehaviour
+    {
+        public GameObjectVariable NoteUI;
+        public NoteScriptable note;
 
+        public void SetNoteUIActive(bool on)
+        {
+            NoteUI.Value.transform.GetChild(0).gameObject.SetActive(on);
+            NoteUI.Value.transform.GetChild(1).gameObject.SetActive(on);
+            NoteUI.Value.transform.GetChild(2).gameObject.SetActive(on);
+        }
+
+        public void SetUIText()
+        {
+            note.SetIsEnabled(true);
+            var textUI = NoteUI.Value.transform.GetChild(1);
+            var text = textUI.GetComponent<Text>();
+            text.text = note.data;
+        }
+    }
 }
--- Assets/Scripts/Zach/NoteUIBehaviour.cs
+++ Assets/Scripts/Zach/NoteUIBehaviour.cs
@@ -3,36 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Zach;
+using Matthew;
 
-public class NoteUIBehaviour : MonoBehaviour
+namespace Zach
 {
-    public GameObject NotePopUp;
-    public NoteScriptable note;
-    private Button bttn;
-    public void Start()
+    public class NoteUIBehaviour : MonoBehaviour
     {
-        bttn = GetComponent<Button>();
-        var textObject = this.transform.GetChild(0);
-        var textComp = textObject.GetComponent<Text>();
-        textComp.text = note.noteName;
-    }
+        public GameObjectVariable NotePopUp;
+        public NoteScriptable note;
+        private Button bttn;
+        public void Start()
+        {
+            bttn = GetComponent<Button>();
+            var textObject = this.transform.GetChild(0);
+            var textComp = textObject.GetComponent<Text>();
+            textComp.text = note.noteName;
+        }
 
-    public void Update()
-    {
-        bttn.interactable = note.GetIsEnabled();
-    }
-    public void SetText(Text textUI)
-    {
-        textUI.text = note.data;
-    }
+        public void Update()
+        {
+            bttn.interactable = note.GetIsEnabled();
+        }
+        public void SetText(Text textUI)
+        {
+            textUI.text = note.data;
+        }
 
-    public void OnClick()
-    {
-        
-        NotePopUp.SetActive(true);
-        var popUpChild = NotePopUp.transform.GetChild(0);
-        var popUpText = popUpChild.GetComponent<Text>();
-        popUpText.text = note.data;
+        public void OnClick()
+        {
 
+            NotePopUp.Value.transform.GetChild(0).gameObject.SetActive(true);
+            var PopUpTextChild = NotePopUp.Value.transform.GetChild(1);
+            PopUpTextChild.gameObject.SetActive(true);
+            NotePopUp.Value.transform.GetChild(2).gameObject.SetActive(true);
+            var popUpText = PopUpTextChild.GetComponent<Text>();
+            popUpText.text = note.data;
+
+        }
     }
 }
