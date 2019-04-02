1b5062afcce284be88d11b27babda70de077dc9f
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-04-02 14:36:39 -0500

    working on waypoint system

---
+++ Assets/Scripts/Luke/OnWaypointEnterBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Luke
+{
+    public class OnWaypointEnterBehaviour : MonoBehaviour
+    {
+        public UnityEvent Response;
+
+        public void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Response.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Luke/WaypointBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Matthew;
+using UnityEngine.Events;
+using UnityEngine.Experimental.Audio.Google;
+
+
+namespace Luke
+{
+    public class WaypointBehaviour : MonoBehaviour
+    {
+        public GameObjectVariable player_reference;
+        public List<Transform> waypoints;
+        public bool is_spawned = false;
+        [HideInInspector] public Transform current_waypoint;
+        public GameEvent waypoint_reached;
+        public UnityEvent Response;
+
+
+        private void Start()
+        {
+            Response.Invoke();
+        }
+
+        public void SpawnPlayer(Transform waypoint)
+        {
+            Instantiate(player_reference);
+            is_spawned = true;
+            player_reference.Value.transform.position = waypoint.position;
+        }
+
+        public void MoveCurrentWaypoint()
+        {
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                if (current_waypoint == null)
+                {
+                    current_waypoint = waypoints[0];
+                }
+                else
+                {
+                    current_waypoint = waypoints[i];
+                }
+                Debug.Log("current waypoint is " + current_waypoint.gameObject.name);
+            }
+        }
+    }
+}
