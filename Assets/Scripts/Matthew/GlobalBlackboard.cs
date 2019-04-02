64800cf53bea39bc29455fe11580008ae12a3fb3
Matthew Williamson <chuwilliamson@gmail.com>
2019-04-02 13:04:55 -0500

    global object to invoke functions with, can be used currently to instantiate an object and set the players position

---
+++ Assets/Scripts/Matthew/GlobalBlackboard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Matthew
+{
+    [CreateAssetMenu]
+    public class GlobalBlackboard : ScriptableObject
+    {
+        [SerializeField]
+        GameObjectVariable PlayerReference;
+        public void InstantiateObject(GameObject prefab)
+        {
+            Instantiate(prefab);
+        }
+        public void SetPlayerPosition(Transform transform)
+        {
+            PlayerReference.Transform.position = transform.position;
+        }
+    }
+}
