e8d416f31bf5127caa5230f1912689288ed55686
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-04-04 14:20:53 -0500

    player no longer faces the camera all the time instead the place faces the direction they are moving relative to the camera

--- Assets/Scripts/Zach/NewPlayerMovementBehaviour.cs
+++ Assets/Scripts/Zach/NewPlayerMovementBehaviour.cs
@@ -8,12 +8,16 @@ namespace Zach
     public class NewPlayerMovementBehaviour : MonoBehaviour
     {
         private Vector3 _moveVector;
+        public Vector3 MoveVector
+        {
+            get { return _moveVector; }
+        }
         public float speed = 5;
         public float jumpPower = 4;
         public float gravity = 9.81f;
-        private CharacterController _controller;
         public Vector3 camRight;
         public Vector3 camForward;
+        private CharacterController _controller;
         public bool isFrozen = false;
 
         private void Start()
@@ -24,45 +28,64 @@ namespace Zach
         // Update is called once per frame
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                Application.Quit();
-            }
-
             if (!isFrozen)
             {
+                var h = PlayerInput.InputVector.normalized.x;
+                var v = PlayerInput.InputVector.normalized.z;
+                var forward = Camera.main.transform.TransformDirection(Vector3.forward);
+                forward.y = 0;
+                forward = forward.normalized;
+                var right = new Vector3(forward.z, 0, -forward.x);
+                var targetDir = h * right + v * forward;
+                if (targetDir.magnitude > 0)
+                    transform.rotation = Quaternion.LookRotation(targetDir);
+                _moveVector.x = targetDir.x;
+                _moveVector.z = targetDir.z;
+                _moveVector.y = _moveVector.y - (gravity * Time.deltaTime);
                 if (_controller.isGrounded)
                 {
-                    if (Camera.main != null)
+                    if (Input.GetButtonDown("Jump"))
                     {
-                        var transform3 = Camera.main.transform;
-                        camRight = transform3.right;
-                        camForward = transform3.forward;
-                    }
-
-                    camForward *= PlayerInput.InputVector.z;
-                    camRight *= PlayerInput.InputVector.x;
-
-                    _moveVector = (camForward + camRight);
-                    var right = new Vector3(camForward.z, 0, -camForward.x);
-                    if (Input.GetKeyDown(KeyCode.Space))
-                    {
-                        _moveVector += new Vector3(0,jumpPower,0);
+                        _moveVector.y = jumpPower;
                     }
                 }
-
-                transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * 180 * Time.deltaTime);
-                _moveVector.y = _moveVector.y - (gravity * Time.deltaTime);
-                var transform1 = transform;
-                if (Camera.main != null)
-                {
-                    var transform2 = Camera.main.transform;
-                    var forward = transform2.forward;
-                    transform1.forward = new Vector3(forward.x, transform1.forward.y,
-                        forward.z);
-                }
-
                 _controller.Move(_moveVector * speed * Time.deltaTime);
+
+                //if (!isFrozen)
+                //{
+                //    if (_controller.isGrounded)
+                //    {
+                //        if (Camera.main != null)
+                //        {
+                //            var transform3 = Camera.main.transform;
+                //            camRight = transform3.right;
+                //            camForward = transform3.forward;
+                //        }
+
+                //        camForward *= PlayerInput.InputVector.z;
+                //        camRight *= PlayerInput.InputVector.x;
+
+                //        _moveVector = (camForward + camRight);
+                //        var right = new Vector3(camForward.z, 0, -camForward.x);
+                //        if (Input.GetKeyDown(KeyCode.Space))
+                //        {
+                //            _moveVector += new Vector3(0, jumpPower, 0);
+                //        }
+                //    }
+
+                //    transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * 180 * Time.deltaTime);
+                //    _moveVector.y = _moveVector.y - (gravity * Time.deltaTime);
+                //    var transform1 = transform;
+                //    if (Camera.main != null)
+                //    {
+                //        var transform2 = Camera.main.transform;
+                //        var forward = transform2.forward;
+                //        transform1.forward = new Vector3(forward.x, transform1.forward.y,
+                //            forward.z);
+                //    }
+
+                //    _controller.Move(_moveVector * speed * Time.deltaTime);
+                //}
             }
         }
     }
