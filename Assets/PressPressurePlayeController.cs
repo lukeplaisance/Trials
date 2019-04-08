64fbaebe26a4a960f94d5359fe7b44721379376d
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-04-08 16:57:02 -0500

    created assets to move the pressure plate

---
+++ Assets/PressPressurePlayeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PressPressurePlayeController : MonoBehaviour
+{
+    public float speed;
+    public void MovePressurePlate()
+    {
+        var a = GetComponent<Animator>();
+        a.SetBool("isMoving", true);
+        StartCoroutine("Move");
+        transform.position += new Vector3(0, -.5f, 0) * speed * Time.deltaTime; 
+    }
+
+    IEnumerator Move()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            var a = GetComponent<Animator>();
+            a.SetBool("isMoving", false);
+            break;
+        }
+    }
+}
