643875dd93ac076af13090fa806c3eb84c91870b
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-04-09 17:48:52 -0500

    worked on improving player movement

--- Assets/Scripts/Zach/NewPlayerMovementBehaviour.cs
+++ Assets/Scripts/Zach/NewPlayerMovementBehaviour.cs
@@ -8,15 +8,18 @@ namespace Zach
     public class NewPlayerMovementBehaviour : MonoBehaviour
     {
         [SerializeField]
-        private Vector3 _moveVector;
+        private Vector3 _moveVector = Vector3.zero;
         private Vector3 _prevMoveVector;
         public Vector3 MoveVector
         {
             get { return _moveVector; }
         }
         public float speed = 5;
+        private float baseSpeed;
         public float jumpPower = 4;
         public float gravity = 9.81f;
+        public float mag;
+        public float _prevMag;
         public Vector3 velocity;
         public Vector3 camRight;
         public Vector3 camForward;
@@ -29,17 +32,34 @@ namespace Zach
 
         private void Start()
         {
+            baseSpeed = speed;
             _prevPosition = transform.position;
             _controller = GetComponent<CharacterController>();
             isGrounded = _controller.isGrounded;
             _animator = GetComponent<Animator>();
+            mag = PlayerInput.InputVector.magnitude;
         }
 
         // Update is called once per frame
         private void Update()
         {
+            speed = baseSpeed;
+            mag = PlayerInput.InputVector.magnitude;
+            if (mag > 0.20f)
+            {
+                if (_prevMag < PlayerInput.InputVector.magnitude)
+                    _prevMag += Time.deltaTime;
+            }
+            else if (mag < 0.20f)
+            {
+                if (_prevMag > 0)
+                {
+                    _prevMag -= Time.deltaTime;
+                }
+            }
             if (!isFrozen)
             {
+                
                 var h = PlayerInput.InputVector.normalized.x;
                 var v = PlayerInput.InputVector.normalized.z;
                 var forward = Camera.main.transform.TransformDirection(Vector3.forward);
@@ -49,13 +69,20 @@ namespace Zach
                 var targetDir = h * right + v * forward;
                 if (targetDir.magnitude > 0)
                 {
-                    //transform.rotation = Quaternion.LookRotation(targetDir);
                     var rot = Quaternion.Euler(targetDir);
                     transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(targetDir),0.25f);
                 }
-                _moveVector.x = targetDir.x;
-                _moveVector.z = targetDir.z;
-                if(!_controller.isGrounded)
+                _moveVector.x += targetDir.x;
+                if (_moveVector.x >= 5)
+                    _moveVector.x = 5;
+                _moveVector.z += targetDir.z;
+                if (_moveVector.z >= 5)
+                {
+                    _moveVector.z = 5;
+                }
+
+
+                if (!_controller.isGrounded)
                 {
                     _moveVector.y = _moveVector.y - (gravity * Time.deltaTime);
                 }
@@ -66,7 +93,12 @@ namespace Zach
                     Debug.Log("Test");
                     _animator.SetTrigger("OnJump");
                 }
-                _controller.Move(_moveVector * speed * Time.deltaTime);
+                speed *= _prevMag;
+                _moveVector.x *= speed;
+                _moveVector.z *= speed;
+                _controller.Move(_moveVector * Time.deltaTime);
+                _moveVector.x -= targetDir.x;
+                _moveVector.z -= targetDir.z;
             }
             velocity = (transform.position - _prevPosition) / Time.deltaTime;
             _animator.SetFloat("Velocity",_controller.velocity.magnitude);
@@ -76,38 +108,3 @@ namespace Zach
         }
     }
 }
-//if (!isFrozen)
-//{
-//    if (_controller.isGrounded)
-//    {
-//        if (Camera.main != null)
-//        {
-//            var transform3 = Camera.main.transform;
-//            camRight = transform3.right;
-//            camForward = transform3.forward;
-//        }
-
-//        camForward *= PlayerInput.InputVector.z;
-//        camRight *= PlayerInput.InputVector.x;
-
-//        _moveVector = (camForward + camRight);
-//        var right = new Vector3(camForward.z, 0, -camForward.x);
-//        if (Input.GetKeyDown(KeyCode.Space))
-//        {
-//            _moveVector += new Vector3(0, jumpPower, 0);
-//        }
-//    }
-
-//    transform.Rotate(Vector3.up, Input.GetAxis("Mouse X") * 180 * Time.deltaTime);
-//    _moveVector.y = _moveVector.y - (gravity * Time.deltaTime);
-//    var transform1 = transform;
-//    if (Camera.main != null)
-//    {
-//        var transform2 = Camera.main.transform;
-//        var forward = transform2.forward;
-//        transform1.forward = new Vector3(forward.x, transform1.forward.y,
-//            forward.z);
-//    }
-
-//    _controller.Move(_moveVector * speed * Time.deltaTime);
-//}
