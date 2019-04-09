aa3ffda94760abe12723e9dc40f30f3176b12fa9
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-04-09 10:50:45 -0500

    created scripts to raise game event on trigger enter and on trigger exit

---
+++ Assets/Scripts/Luke/RaiseGameEventOnTriggerExit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Luke
+{
+    public class RaiseGameEventOnTriggerExit : MonoBehaviour
+    {
+        [SerializeField]
+        private GameEvent gameEvent;
+        public string colliderTag;
+
+        public void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag(colliderTag))
+            {
+                gameEvent.Raise();
+            }
+        }
+    }
+}
