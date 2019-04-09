693693fd710d25b0c51c2698a61d07abf4450289
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-04-09 10:51:12 -0500

    created script to handle the animations for the pressure plate

---
+++ Assets/Scripts/Luke/PressurePlateTriggerBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Luke
+{
+    public class PressurePlateTriggerBehaviour : MonoBehaviour
+    {
+        public Animator animator;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Grabbable"))
+            {
+                animator.SetBool("IsDown", true);
+                animator.Play("MovePressurePlateDown");
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Grabbable"))
+            {
+                animator.SetBool("IsDown", false);
+                animator.Play("MovePressurePlateUp");
+            }
+        }
+    }
+}
