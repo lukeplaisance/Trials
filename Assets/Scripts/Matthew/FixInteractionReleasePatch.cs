cea8a705f936feb8e98b13daa21975d8bb689e70
Matthew Williamson <chuwilliamson@gmail.com>
2019-04-11 18:12:41 -0500

    add patch script to address the releasing of interaction when we stop an interaction

---
+++ Assets/Scripts/Matthew/FixInteractionReleasePatch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Luke;
+using UnityEngine;
+
+public class FixInteractionReleasePatch : MonoBehaviour
+{
+    public List<InteractionBehaviour> InteractableBehaviours;
+	// Use this for initialization
+	void Start ()
+    {
+        InteractableBehaviours = GetComponentsInChildren<InteractionBehaviour>().ToList();
+        InteractableBehaviours.ForEach(ib => ib.InteractStopResponse.AddListener(ib.ReleaseInteraction));
+    }
+	
+ 
+}
