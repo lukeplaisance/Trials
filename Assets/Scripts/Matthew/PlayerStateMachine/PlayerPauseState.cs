0ded9ead5664be93fb1c6e49abacfd77a17829df
Matthew Williamson <chuwilliamson@gmail.com>
2019-04-11 16:33:48 -0500

    move states into seperate files

---
+++ Assets/Scripts/Matthew/PlayerStateMachine/PlayerPauseState.cs
@@ -0,0 +1,31 @@
+﻿namespace Matthew
+{
+    public class PlayerPauseState : IState
+    {
+        StateEventTransitionSubscription listener;
+        public void OnEnter(IContext context)
+        {
+            var playerbehaviour = (context as PlayerContext).Behaviour;
+            playerbehaviour.SetMovement(false);
+            listener = new StateEventTransitionSubscription
+            {
+                Subscribeable = UnityEngine.Resources.Load("Events/ClosePauseMenu") as Luke.GameEvent
+            };
+        }
+ 
+
+        public void OnExit(IContext context)
+        {
+            var playerbehaviour = (context as PlayerContext).Behaviour;
+            playerbehaviour.SetMovement(true);
+            
+        }
+
+        public void UpdateState(IContext context)
+        {
+            if (listener.EventRaised)
+                context.ChangeState(new PlayerIdleState());
+
+        }
+    }
+}
