fac4f850e72fa1a4fae8d7dedd132ea7e140c496
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-04-16 14:22:10 -0500

    script to have the button promt always looking at the camera

---
+++ Assets/CanvasBillBoardBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using Matthew;
+using UnityEngine;
+
+namespace Luke
+{
+    public class CanvasBillBoardBehaviour : MonoBehaviour
+    {
+        public Cinemachine.CinemachineFreeLook freelook_camrea;
+
+        void Update()
+        {
+            transform.LookAt(freelook_camrea.transform.position);
+        }
+    }
+}
