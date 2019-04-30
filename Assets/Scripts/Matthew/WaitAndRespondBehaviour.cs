25fda1fb777ca038c77a0454bdf7dc928971243a
Matthew Williamson <chuwilliamson@gmail.com>
2019-04-30 15:32:19 -0500

    Interactable refreshed by removing empty delegate fields on unityevent responses 0.main for prefab changes InteractionBehaviour added comments PlayerInteractState added button for exiting the interact state Interactable-Pickup.prefab wait timer version of interaction WaitAndRespondBehaviour behaviour to wait then call a function

---
+++ Assets/Scripts/Matthew/WaitAndRespondBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitAndRespondBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent Response;
+    public void WaitAndRespond(float time)
+    {
+        StartCoroutine(WaitAndDo(time));
+    }
+    private IEnumerator WaitAndDo(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Response.Invoke();
+    }
+}
