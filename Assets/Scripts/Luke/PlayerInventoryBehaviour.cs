9d50f1f974891b0e1981c7bb1ea634980bf3a286
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-02 11:26:52 -0500

    created an inventory to hold the items being picked up

---
+++ Assets/Scripts/Luke/PlayerInventoryBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+namespace Luke
+{
+    public class PlayerInventoryBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private List<Item> Player_Inventory = new List<Item>();
+        public bool has_item = false;
+
+        //void Start()
+        //{
+        //    Player_Inventory.
+        //}
+
+        public void AddItem(Item item)
+        {
+            Player_Inventory.Add(item);
+            Debug.Log("picked up" + item.name);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            if (has_item)
+            {
+                Player_Inventory.Remove(item);
+                Debug.Log("dropped" + item.name);
+            }
+        }
+
+        public void CheckForItem(Item item)
+        {
+            if (Player_Inventory.Contains(item))
+            {
+                has_item = true;
+            }
+        }
+    }
+}
