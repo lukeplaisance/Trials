50bc938f14b5b8943116b62bfc1713259f68c833
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-02 15:34:52 -0500

    now has references to the PlayerInventoryObject

--- Assets/Scripts/Luke/PlayerInventoryBehaviour.cs
+++ Assets/Scripts/Luke/PlayerInventoryBehaviour.cs
@@ -2,41 +2,22 @@
 using System.Collections.Generic;
 using System.Security.Policy;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Luke
 {
     public class PlayerInventoryBehaviour : MonoBehaviour
     {
-        [SerializeField]
-        private List<Item> Player_Inventory = new List<Item>();
-        public bool has_item = false;
-
-        //void Start()
-        //{
-        //    Player_Inventory.
-        //}
+        [SerializeField] private PlayerInventoryObject PlayerInventoryScriptableObject;
 
         public void AddItem(Item item)
         {
-            Player_Inventory.Add(item);
-            Debug.Log("picked up" + item.name);
+            PlayerInventoryScriptableObject.AddItem(item);
         }
 
         public void RemoveItem(Item item)
         {
-            if (has_item)
-            {
-                Player_Inventory.Remove(item);
-                Debug.Log("dropped" + item.name);
-            }
-        }
-
-        public void CheckForItem(Item item)
-        {
-            if (Player_Inventory.Contains(item))
-            {
-                has_item = true;
-            }
+            PlayerInventoryScriptableObject.RemoveItem(item);
         }
     }
 }
