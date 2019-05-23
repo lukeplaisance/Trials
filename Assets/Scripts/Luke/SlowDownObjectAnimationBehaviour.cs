d97b268f49adb085c221fff5741e7b2ba560e1b6
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-23 10:29:04 -0500

    started on slowing down object animation speed

---
+++ Assets/Scripts/Luke/SlowDownObjectAnimationBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Luke
+{
+    public class SlowDownObjectAnimationBehaviour : MonoBehaviour
+    {
+        public Animator animator;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        public void SlowDownObjectAnimation()
+        {
+            animator.speed = (animator.speed / 2);
+        }
+    }
+}
