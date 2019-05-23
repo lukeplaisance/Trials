b2ec1ad91f3d6c1854ec1fe2a1ed8463878d83af
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-23 11:22:08 -0500

    animation slows down if the object is in a certain radius of the player

--- Assets/Scripts/Luke/SlowDownObjectAnimationBehaviour.cs
+++ Assets/Scripts/Luke/SlowDownObjectAnimationBehaviour.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Matthew;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngineInternal;
 
 
 namespace Luke
@@ -8,15 +11,44 @@ namespace Luke
     public class SlowDownObjectAnimationBehaviour : MonoBehaviour
     {
         public Animator animator;
+        public GameObjectVariable player;
+        public float threshold;
+        public bool in_range;
+        public UnityEvent Response;
 
         void Start()
         {
             animator = GetComponent<Animator>();
         }
 
-        public void SlowDownObjectAnimation()
+        public void SlowDownObjectAnimation(float speed)
         {
-            animator.speed = (animator.speed / 2);
+            if (in_range)
+            {
+                animator.speed = speed;
+            }
+        }
+
+        public void CheckDistanceofPlayer()
+        {
+            if(Vector3.Distance(gameObject.transform.position, player.Transform.position) < threshold)
+            {
+                Debug.Log("Player is in range");
+                in_range = true;
+                SlowDownObjectAnimation(0.5f);
+            }
+            else
+            {
+                in_range = false;
+                animator.speed = 1;
+                return;
+            }
+        }
+
+        void Update()
+        {
+            Debug.Log(animator.speed);
+            Response.Invoke();
         }
     }
 }
