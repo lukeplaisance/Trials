f539b9e97565e9324e98b140e9bc00b2a2862e15
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-30 16:25:34 -0500

    removed range check

--- Assets/Scripts/Luke/ChangeObjectAnimationSpeedBehaviour.cs
+++ Assets/Scripts/Luke/ChangeObjectAnimationSpeedBehaviour.cs
@@ -11,9 +11,6 @@ namespace Luke
     public class ChangeObjectAnimationSpeedBehaviour : MonoBehaviour
     {
         public Animator animator;
-        public GameObjectVariable player; //static reference to the playre prefab
-        public float animation_speed; //cut the speed in half
-        public bool in_range;
         public UnityEvent play_animation;
 
         void Start()
@@ -23,32 +20,12 @@ namespace Luke
 
         public void SlowDownObjectAnimation(float speed)
         {
-            if (in_range)
-            {
-                animator.speed = speed;
-            }
+            animator.speed = speed;
         }
 
         public void ReturnObjectAnimationSpeed()
         {
             animator.speed = 1;
-        }
-
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("Astrolabe"))
-            {
-                Debug.Log("Player is in range");
-                in_range = true;
-                SlowDownObjectAnimation(animation_speed);
-            }
-        }
-
-        void OnTriggerExit(Collider other)
-        {
-            in_range = false;
-            animator.speed = 1;
-            return;
         }
 
         void Update()
