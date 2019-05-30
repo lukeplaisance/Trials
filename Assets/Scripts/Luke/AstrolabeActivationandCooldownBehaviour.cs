1485964f7f8e95d9e4c953fb36e95110eceabd33
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-30 16:24:23 -0500

    created script for astrolabe activation

---
+++ Assets/Scripts/Luke/AstrolabeActivationandCooldownBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Luke
+{
+    public class AstrolabeActivationandCooldownBehaviour : MonoBehaviour
+    {
+        public UnityEvent Activate;
+
+        public float animation_speed;
+        public bool in_range;
+        private ChangeObjectAnimationSpeedBehaviour[] found_objects;
+        [SerializeField] private List<ChangeObjectAnimationSpeedBehaviour> object_list;
+
+
+        void Start()
+        {
+            found_objects = FindObjectsOfType<ChangeObjectAnimationSpeedBehaviour>();
+        }
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire4"))
+            {
+                Activate.Invoke();
+            }
+        }
+
+        public void AddFoundAnimationObjectsToList()
+        {
+            foreach (var obj in found_objects)
+            {
+                object_list.Add(obj);
+            }
+
+            foreach (var obj in object_list)
+            {
+                if (in_range)
+                {
+                    obj.SlowDownObjectAnimation(animation_speed);
+                }
+            }
+        }
+
+        public void RemoveFoundAnimationObjects()
+        {
+            foreach (var obj in object_list)
+            {
+                obj.ReturnObjectAnimationSpeed();
+            }
+
+            foreach (var obj in found_objects)
+            {
+                object_list.Remove(obj);
+            }
+
+        }
+    }
+}
