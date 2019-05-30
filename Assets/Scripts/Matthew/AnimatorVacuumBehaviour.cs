b521ae2d06db25a276c23acb0ad030f35ded2148
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-30 16:25:14 -0500

    script to find animators throughout the level and add them to a list

---
+++ Assets/Scripts/Matthew/AnimatorVacuumBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Luke;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AnimatorVacuumBehaviour : MonoBehaviour
+{
+    public List<Animator> Animators = new List<Animator>();
+
+    public void AddToList(Animator anim)
+    {
+        if (Animators.Contains(anim))
+            return;
+        Animators.Add(anim);
+    }
+
+    public void RemoveFromList(Animator anim)
+    {
+        if (!Animators.Contains(anim))
+            return;
+        Animators.Remove(anim);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        var animator = other.GetComponent<Animator>();
+        if (animator != null)
+        {
+            AddToList(animator);
+        }
+    }
+}
