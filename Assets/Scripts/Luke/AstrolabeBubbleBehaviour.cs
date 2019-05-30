e2936f844a02b6f28c15c2dadb926867cb2e690c
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-30 16:24:50 -0500

    created script for the astrolabe bubble

---
+++ Assets/Scripts/Luke/AstrolabeBubbleBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Luke
+{
+    public class AstrolabeBubbleBehaviour : MonoBehaviour
+    {
+
+        public AstrolabeActivationandCooldownBehaviour astrolabe_bubble;
+
+        void Start()
+        {
+            astrolabe_bubble = FindObjectOfType<AstrolabeActivationandCooldownBehaviour>();
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Animation_Object"))
+            {
+                astrolabe_bubble.in_range = true;
+                astrolabe_bubble.AddFoundAnimationObjectsToList();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            astrolabe_bubble.in_range = false;
+            astrolabe_bubble.RemoveFoundAnimationObjects();
+        }
+    }
+}
