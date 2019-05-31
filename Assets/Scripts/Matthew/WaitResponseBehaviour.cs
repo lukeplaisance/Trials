6d2d9f54dfcd811740fc8af575caf0085e57aca1
Matthew Williamson <chuwilliamson@gmail.com>
2019-05-31 17:28:15 -0500

    Add files via upload

--- Assets/Scripts/Matthew/WaitResponseBehaviour.cs
+++ Assets/Scripts/Matthew/WaitResponseBehaviour.cs
@@ -1,30 +1,32 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// use this class to make a sequence of events happen after a specified time
-/// </summary>
-public class WaitResponseBehaviour : MonoBehaviour
+namespace Matthew
 {
-    private int _currentIndex;
-    public List<WaitResponse> Responses;
-
-
-    public void Execute()
+    /// <summary>
+    ///     use this class to make a sequence of events happen after a specified time
+    /// </summary>
+    public class WaitResponseBehaviour : MonoBehaviour
     {
-        //for every response
-        foreach (var response in Responses)
-            //when it is done executing
-            response.OnDone += () =>
-            {
-                //increment the current index 
-                _currentIndex++;
-                //and if the current index is not past the last item
-                if (_currentIndex + 1 <= Responses.Count)
-                    //call the next function inside the ondone
-                    Responses[_currentIndex].Invoke(this);
-            };
+        private int _currentIndex;
+        public List<WaitResponse> responses;
 
-        Responses[_currentIndex].Invoke(this);
+        public void Execute()
+        {
+            //for every response
+            foreach (var response in responses)
+                //when it is done executing
+                response.OnDone += () =>
+                {
+                    //increment the current index 
+                    _currentIndex++;
+                    //and if the current index is not past the last item
+                    if (_currentIndex + 1 <= responses.Count)
+                        //call the next function inside the OnDone
+                        responses[_currentIndex].Invoke(this);
+                };
+
+            responses[_currentIndex].Invoke(this);
+        }
     }
 }
