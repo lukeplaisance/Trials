35f06547696fff550d310b743079630f4c380481
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-05-31 17:30:46 -0500

    removed slow down and return speed methods

---
+++ Assets/Scripts/Luke/AstrolabeBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+namespace Luke
+{
+    public class AstrolabeBehaviour : MonoBehaviour
+    {
+        public GameEvent Activate_Astrolabe;
+        public GameEvent Cooldown_Astrolabe;
+
+        public UnityEvent Activate;
+
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire4"))
+            {
+                Activate.Invoke();
+                Debug.Log(Activate_Astrolabe + "Raised");
+            }
+        }
+
+        void OnDisable()
+        {
+            Debug.Log(Cooldown_Astrolabe + "Raised");
+        }
+    }
+}
