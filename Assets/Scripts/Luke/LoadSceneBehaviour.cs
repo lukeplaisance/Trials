07af5f7e1658f4270b4aa341f8e725fd6b5e872a
lukeplaisance <31013260+lukeplaisance17@users.noreply.github.com>
2019-06-11 09:14:16 -0500

    created script to load a scene

---
+++ Assets/Scripts/Luke/LoadSceneBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneBehaviour : MonoBehaviour
+{
+    public void LoadNewScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
