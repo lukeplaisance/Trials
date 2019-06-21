769d164ee69fe28177cfeca923936a6ecab59f0c
Sharkanoid <bryanevans712@yahoo.com>
2019-06-21 18:03:02 -0500

    Saving Collision Changes

---
+++ Assets/Scripts/Matthew/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public List<GameObject> colliderHolders = new List<GameObject>();
+
+    [ContextMenu("Create PlaceHolder Colliders")]
+    public void CreateGameObjects()
+    {
+        var selected = Selection.activeObject as GameObject;
+        var colliders = selected.GetComponents<BoxCollider>();
+        foreach(var col in colliders)
+        {
+            var go = Instantiate(new GameObject(), Vector3.zero, Quaternion.identity, selected.transform);
+            var go_col = go.AddComponent<BoxCollider>();
+            EditorUtility.CopySerialized(col, go_col);
+            colliderHolders.Add(go);
+        }
+    }
+}
