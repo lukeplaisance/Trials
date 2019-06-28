96bfa5aeaf55d623511fea1610a193ac8f72fccd
ZacharyBriggs <zacharybriggs64@gmail.com>
2019-06-28 16:30:56 -0500

    made script for title screen to switch scenes

---
+++ Assets/Scripts/Zach/OnPressABehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Zach
+{
+    public class OnPressABehaviour : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                SceneManager.LoadScene("0.main");
+            }
+        }
+    }
+}
